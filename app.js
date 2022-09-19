b23860c37645b20c763b537a894e043181aad474
raianil5545 <raianil9738@gmail.com>
2022-09-19 18:04:04 +0545

    creating the project and app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+require("dotenv").config()
+
+
+const app = express();
+
+app.listen(process.env.PORT, (err, data) => {
+    if (err){
+        console.log(err)
+    }
+    else{
+        console.log("... listening ....")
+    }
+})
